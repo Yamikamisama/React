a70c7d265fe53129491556c78d2048575bedcc26
Michael Angelo <yamikamisama@gmail.com>
2015-06-24 12:38:58 -0400

    basic functionallity incorperated

---
+++ shut_the_box/public/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	dieTotal = null;
+
+	// Event Lister for the cards to create the border
+	// color change
+	$('.cards').on('click', 'li', function(){
+		var active = $(this).hasClass('card-active')
+		if(!active){
+			$(this).addClass('card-active');
+		} else {
+			$(this).removeClass('card-active');
+		}
+	});
+
+
+  // Event Listener to determine whether the end turn
+  // Is valid or not
+  $('#end-turn').click(function(){
+  	var $cards = $('.card')
+  	var holder = []
+  	$cards.each(function(){
+  		var active = $(this).hasClass('card-active')
+  		if (active){
+  			var item = parseInt($(this).html())
+  			holder.push(item)
+  		}
+  	});
+  	var total = holder.reduce(function(a, b) {
+  		return a + b;
+  	});
+  	if (total % dieTotal === 0){ $('.card-active').removeClass('card-active').hide();}
+  	debugger
+
+  });
+
+  // Event Listener that will roll the Dice and display the
+  // outcome to the user
+  $('#die-roll').click(function() {
+  	dieTotal = 0;
+  	$('.die').each(function(){
+  		var randNum = (Math.floor(Math.random() * 6) + 1)
+  		var roll = $(this).html(randNum)
+  		dieTotal += randNum;
+  		debugger
+  	});
+  });
+});
+
