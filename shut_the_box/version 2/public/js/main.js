b4178cc0e9460a5e81b6d044bb5dd4fcb3381907
Michael Angelo <yamikamisama@gmail.com>
2015-06-25 08:42:05 -0400

    move functions to sequential order

--- shut_the_box/version 2/public/js/main.js
+++ shut_the_box/version 2/public/js/main.js
@@ -17,13 +17,16 @@ STB = {
     $(element).append('<ul class="cards"><li class="card">1</li><li class="card">2</li><li class="card">3</li><li class="card">4</li><li class="card">5</li><li class="card">6</li><li class="card">7</li><li class="card">8</li><li class="card">9</li></ul><br><br><button id="end-turn" class="btn">End Turn</button><div class="dice-container"><div class="die"></div><div class="die"></div></div><button id="die-roll" class="btn">Roll</button><button id="end-game" class="btn">End Game</button><div class="win"><h1>Congrats you win!</h1></div><div class="loose"><h1>Sorry you loose :( The game will now reload</h1></div><div class="error"><h1>Incorrect selection, try again</h1></div><div class="success"><h1>Good Move! Keep Going</h1></div>')
   },
 
-  // Displays Messages based on ClassName
-  // .win, .loose, .error, .success
-  messageDisplay: function(className){
-    $(className).fadeIn('slow', function() {
-      setTimeout(function(){
-        $(className).fadeOut('slow')
-      }, 2000)
+  // Rolls the dice and stores the sum of the roll
+  // in the dieTotal var
+  dieRoll: function(){
+    $('#die-roll').click(function() {
+      dieTotal = 0;
+      $('.die').each(function(){
+        var randNum = (Math.floor(Math.random() * 6) + 1)
+        var roll = $(this).html('<img src="imgs/dice/die_'+ randNum + '.png" alt="">')
+        dieTotal += randNum;
+      });
     });
   },
 
@@ -38,6 +41,23 @@ STB = {
         $(this).removeClass('card-active');
       }
     });
+  },
+
+  // Updates cards current 'state', removing them if
+  // their total % dieTotal = 0, also displays a message
+  // to the user updating them on what to do next
+  updateCards: function(total){
+    if (total % dieTotal === 0){
+      $('.card-active').removeClass('card-active').remove();
+      if ( $('.card').length === 0 ) {
+        this.messageDisplay('.win');
+        STB.reloadGame();
+      } else {
+        this.messageDisplay('.success');
+      }
+    } else {
+      this.messageDisplay('.error')
+    }
   },
 
   // Ends the current turn and calls updateCards function
@@ -59,36 +79,6 @@ STB = {
     });
   },
 
-  // Updates cards current 'state', removing them if
-  // their total % dieTotal = 0, also displays a message
-  // to the user updating them on what to do next
-  updateCards: function(total){
-    if (total % dieTotal === 0){
-      $('.card-active').removeClass('card-active').remove();
-      if ( $('.card').length === 0 ) {
-        this.messageDisplay('.win');
-        STB.reloadGame();
-      } else {
-        this.messageDisplay('.success');
-      }
-    } else {
-      this.messageDisplay('.error')
-    }
-  },
-
-  // Rolls the dice and stores the sum of the roll
-  // in the dieTotal var
-  dieRoll: function(){
-    $('#die-roll').click(function() {
-      dieTotal = 0;
-      $('.die').each(function(){
-        var randNum = (Math.floor(Math.random() * 6) + 1)
-        var roll = $(this).html('<img src="imgs/dice/die_'+ randNum + '.png" alt="">')
-        dieTotal += randNum;
-      });
-    });
-  },
-
   // Calls a message telling the User that they have ended
   // the game and that it will reload
   endGame: function(){
@@ -96,6 +86,16 @@ STB = {
       $('.loose').fadeIn('slow', function() {
         STB.reloadGame();
       });
+    });
+  },
+
+  // Displays Messages based on ClassName
+  // .win, .loose, .error, .success
+  messageDisplay: function(className){
+    $(className).fadeIn('slow', function() {
+      setTimeout(function(){
+        $(className).fadeOut('slow')
+      }, 2000)
     });
   },
 
