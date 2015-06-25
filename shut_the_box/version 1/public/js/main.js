402e52ced021f7f85c522e66af7e39d186ecde80
Michael Angelo <yamikamisama@gmail.com>
2015-06-25 08:12:49 -0400

    add version 2, a module version that you can include into your site

---
+++ shut_the_box/version 1/public/js/main.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+	dieTotal = null;
+
+	// Event Lister for the cards to create the border
+	// color change
+	$('.cards').on('click', 'li', function(){
+		var active = $(this).hasClass('card-active')
+		if(!active){
+			$(this).addClass('card-active');
+		} else {
+			$(this).removeClass('card-active');
+		}
+	});
+
+
+  // Event Listener to determine whether the end turn
+  // Is valid or not
+  $('#end-turn').click(function(){
+  	var $cards = $('.card')
+  	var holder = []
+  	$cards.each(function(){
+  		var active = $(this).hasClass('card-active')
+  		if (active){
+  			var item = parseInt($(this).html())
+  			holder.push(item)
+  		}
+  	});
+  	var total = holder.reduce(function(a, b) {
+  		return a + b;
+  	});
+
+  	// Removes cards and displays error handling
+  	if (total % dieTotal === 0){
+  		$('.card-active').removeClass('card-active').remove();
+  	  if ( $('.card').length === 0 ) {
+  			messageDisplay('.win');
+  		} else {
+  			messageDisplay('.success');
+  		}
+  	} else {
+  		messageDisplay('.error')
+  	}
+  });
+
+  // Event Listener that will roll the Dice and display the
+  // outcome to the user
+  $('#die-roll').click(function() {
+  	dieTotal = 0;
+  	$('.die').each(function(){
+  		var randNum = (Math.floor(Math.random() * 6) + 1)
+  		var roll = $(this).html('<img src="imgs/dice/die_'+ randNum + '.png" alt="">')
+  		dieTotal += randNum;
+  	});
+  });
+
+  // Event Listener that ends the game and reloads the page
+  $('#end-game').click(function() {
+	  $('.loose').fadeIn('slow', function() {
+	  	setTimeout(function(){
+	  		$('.loose').fadeOut('slow')
+	  		location.reload();
+	  	}, 2000)
+	  });
+  });
+
+  // Message Display Handler
+  var messageDisplay = function(className){
+  	$(className).fadeIn('slow', function() {
+	  	setTimeout(function(){
+	  		$(className).fadeOut('slow')
+	  	}, 2000)
+	  });
+  }
+});
+
